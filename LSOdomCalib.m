0557d2f0d7daaecd565e752395b9d3a7cb374637
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2025-12-29 10:14:33 +0100

    Commit

--- LSOdomCalib.m
+++ LSOdomCalib.m
@@ -6,32 +6,42 @@ clc
 source "./tools/utilities/geometry_helpers_2d.m"
 #addpath "./exercise/"
 pkg load quaternion
-%h = figure(1);
+h = figure(1);
 
 more off;
 #load the calibration matrix
 disp('Loading data matrix');
 Z=load("./data/dataset_octave.txt");
-#compute the ground truth trajectory
-% plot(Z(:,7),Z(:,8),'r-', 'linewidth', 2)
-% hold on;
+disp('Assing data');
 
-disp('Loading data');
+nominal_params = [0.1 0.0106141 1.4 0];
 
-nominal_params = [0.1 0.0106141 1.4 0] 
-
-encoder_max_values = [8192 5000]
+encoder_max_values = [8192 5000];
 
 %rot = quaternion(config(4,1:4)')
 r_T_l = [[1,0,0,1.5];
          [0,1,0, 0 ];
          [0,0,1, 0 ];
-         [0,0,0, 1 ]]
+         [0,0,0, 1 ]];
+#compute the ground truth trajectory
 
-% TrueTrajectory=compute_odometry_trajectory(Z(:,4:6));
+disp("Display Sensor in laser ground truth")
+
+plot(Z(:,4),Z(:,5),'b-', 'linewidth', 2)
+hold on;
+
+disp("Display robot odometry")
+plot(Z(:,7),Z(:,8),'r-', 'linewidth', 2)
+
+hold on;
+pause(1)
+
+
+
+% TrueTrajectory=compute_odometry_trajectory(Z(:,4:6),Z(:,7:9));
 % disp('ground truth');
-%  hold on;
-%  plot(TrueTrajectory(:,1),TrueTrajectory(:,2), 'r-', 'linewidth', 2);
+% hold on;
+% plot(TrueTrajectory(:,1),TrueTrajectory(:,2), 'g-', 'linewidth', 2);
 % pause(1);
 % nominal_params = [-1;1;0.3];
 % odom = stack_odometry(nominal_params,Z(:,1:2));
@@ -56,4 +66,4 @@ r_T_l = [[1,0,0,1.5];
 % hold on;
 % plot(CalTrajectory(:,1),CalTrajectory(:,2), 'b-', 'linewidth', 2);
 
-%waitfor(h);
+waitfor(h);
