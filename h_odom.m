1b378ed6991369ce0d7d1256911652df3fef78fb
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2026-01-02 19:38:32 +0100

    Now the model should be correct. I was using the front wheel drive model, but x and y were related to rear wheel

--- h_odom.m
+++ h_odom.m
@@ -1,13 +1,21 @@
-function delta = h_odom(x,ticks)
+function delta = h_odom(x,ticks,theta)
 k_s = x(1);
 k_t = x(2);
 steering_offset = x(3);
 l = x(4);
-t_s = ticks(1);
-t_t = ticks(2);
-v = k_t*t_t;
-dphi = k_s*t_s - steering_offset;
-dth = (sin(dphi)/l)*v;
-dx = cos(dth)*cos(dphi)*v;
-dy = sin(dth)*cos(dphi)*v;
-delta = [dx,dy,dth,dphi]';
+t_s = ticks(1); % absolute
+t_t = ticks(2); % incremental
+%t_t%
+phi = mod(k_s*(t_s-steering_offset)+pi,2*pi)-pi;
+ds = k_t*t_t;
+if(ds == 0)
+    delta = [0;0;0];
+
+else
+    
+    dth = (sin(phi)/l)*ds;
+    %dphi = k_s*(t_s - steering_offset);
+    dx = cos(theta+phi)*ds;
+    dy = sin(theta+phi)*ds;
+    delta = [dx;dy;dth];
+end
