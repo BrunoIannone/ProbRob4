f810a9424aa29d50275d0c1d7f6769e10adf275e
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2026-02-11 11:20:21 +0100

    Sync

--- LSOdomCalib.m
+++ LSOdomCalib.m
@@ -4,6 +4,8 @@ clc
 
 #import 2d geometry utils
 source "./tools/utilities/geometry_helpers_2d.m"
+source "./tools/utilities/J_numeric.m"
+
 #addpath "./exercise/"
 pkg load quaternion
 pkg load mapping
@@ -20,7 +22,7 @@ disp('Loading data matrix');
 Z=load("./data/dataset_octave.txt");
 disp('Assign data');
 
-nominal_params = [0.1 0.0106141 1.4 0];
+nominal_params = [0.1 0.0106141 0 1.4];
 
 encoder_max_values = [8192 5000];
 max_incremental_variable = 2^32;
@@ -40,7 +42,7 @@ r_T_l = [[1,0,1.5];
 if(plot_)
     disp("Display robot odometry")
 
-    h1 = plot(robot_odometry_values(:,1),robot_odometry_values(:,2),'b-', 'linewidth', 2);
+    h1 = plot(robot_odometry_values(:,1),robot_odometry_values(:,2),'b-', 'linewidth', 5);
     hold on;
 
     disp("Display sensor GT")
@@ -49,35 +51,40 @@ if(plot_)
     hold on;
 
     disp("Display sensor odometry")
-    h3 = plot(sensor_odometry_values(:,1),sensor_odometry_values(:,2),'y-', 'linewidth', 2);
+    h3 = plot(sensor_odometry_values(:,1),sensor_odometry_values(:,2),'y-', 'linewidth', 5);
 
     hold on;
     
-    incremental_values_rel= get_relative_ticks(incremental_values,max_incremental_variable);
-    
-    attempt = stack_odometry([ 0.1, 0.0106141, 0,1.4],[absolute_values(1:size(absolute_values,1)-1,:),incremental_values_rel], encoder_max_values);
-    disp("Display my robot odometry")
-
-    h4 = plot(attempt(:,1),attempt(:,2), 'g-', 'linewidth', 2);
-    legend([h1 h2 h3 h4], {'Robot odometry', 'Sensor GT', 'Sensor odometry', "My robot odometry"});
-    hold on;
     %legend([h1 h2 h3], {'Robot odometry', 'Sensor GT', 'Sensor odometry'});
-    pause(1)
+
 endif
 
+incremental_values_rel= get_relative_ticks(incremental_values,max_incremental_variable);
+    
+my_robot_odometry = stack_odometry([ 0.1, 0.0106141, 0,1.4],[absolute_values(1:size(absolute_values,1)-1,:),incremental_values_rel], encoder_max_values);
 
-%incremental_values
-% odom = stack_odometry(nominal_params,Z(:,1:2));
-% #compute the uncalibrated odometry
-% OdomTrajectory=compute_odometry_trajectory(odom);
-% disp('odometry');
-% hold on;
-% plot(OdomTrajectory(:,1),OdomTrajectory(:,2), 'g-', 'linewidth', 2);
-% pause(1);
+disp("Display my robot odometry")
+if(plot_)
+    h4 = plot(my_robot_odometry(:,1),my_robot_odometry(:,2), 'g-', 'linewidth', 2);
+    %legend([h1 h2 h3 h4], {'Robot odometry', 'Sensor GT', 'Sensor odometry', "My robot odometry"});
+    hold on;
+endif
 
-% disp('computing calibration parameters');
-%compute the calibration parameters
-%[X,chi]=oneRound(nominal_params,sensor_odometry,Z(:,7:8));
+my_sensor_odometry = compute_sensor_odometry(my_robot_odometry,sensor_translation_wrt_robot);
+if(plot_)
+    
+    h5 = plot(my_sensor_odometry(:,1),my_sensor_odometry(:,2), 'k-', 'linewidth', 2);
+    hold on;
+    legend([h1 h2 h3 h4 h5], {'Robot odometry', 'Sensor GT', 'Sensor odometry', "My robot odometry", "My sensor odometry"});
+
+endif
+
+disp('computing calibration parameters');
+% %compute the calibration parameters
+% [X,chi]=oneRound(nominal_params,[absolute_values(1:size(absolute_values,1)-1,:),incremental_values_rel,Z(1:2433,7:9)]')
+% my_robot_odometry2 = stack_odometry(X',[absolute_values(1:size(absolute_values,1)-1,:),incremental_values_rel], encoder_max_values);
+% h6 = plot(my_robot_odometry2(:,1),my_robot_odometry2(:,2), 'r-', 'linewidth', 10);
+
 %X
 % pause(1);
 
