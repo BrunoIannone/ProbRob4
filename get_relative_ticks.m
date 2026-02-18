dc3b92bd0b519dec55e69ba4107035e1a47e1cb6
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2026-02-18 22:36:35 +0100

    Reworking some old stuff

--- get_relative_ticks.m
+++ get_relative_ticks.m
@@ -1,31 +1,21 @@
 function [incremental]=get_relative_ticks(incremental_values,max_value)
-    threshold  = (max_value*80)/100;  # 4080218931
-
-    overflow_incoming = false;
 
     incremental = zeros(size(incremental_values,1)-1,1);
     
-    for (i = 1:size(incremental_values,1))
+    for (i = 1:size(incremental_values,1)-1)
         
-        if( incremental_values(i,:) > threshold && !overflow_incoming)
-           overflow_incoming = true;
-
-        endif
-        
-        if(i+1 > size(incremental_values,1)) 
-            break
-        endif     
         
         difference = incremental_values(i+1,:) - incremental_values(i,:);
-        
-        
-        if(difference < 0  && overflow_incoming)
-            if(incremental_values(i,:) > threshold && incremental_values(i+1,:)<max_value/2)
-                
+        if difference < 0
+            if(abs(difference) >= max_value/2)   
                 incremental(i,:) = (max_value - incremental_values(i,:)) + incremental_values(i+1,:);
-                overflow_incoming = false;
-            endif
-
+            
+            else
+                incremental(i,:) = difference;
+            end
+        
+        elseif abs(difference) >= max_value/2
+            incremental(i,:) = (incremental_values(i+1,:)- max_value) - incremental_values(i,:)
         
 
         else
@@ -35,4 +25,3 @@ function [incremental]=get_relative_ticks(incremental_values,max_value)
     
     endfor
 endfunction
-    
