54524e8e21f37dcef454d60a4b71f17852c5e87c
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2026-03-06 12:55:20 +0100

    Re-worked relative ticks function

--- compute_relative_ticks.m
+++ compute_relative_ticks.m
@@ -1,23 +1,16 @@
-%Re-adapted from https://www.youtube.com/watch?v=m_2GKHqBmM0
+%Re-working angle normalization formula
 
 function [incremental] = compute_relative_ticks(incremental_values, max_value)
     half_max_value = max_value / 2;
 
-    incremental = zeros(size(incremental_values, 1) - 1, 1);
+    size_ = size(incremental_values, 1) - 1;
+    incremental = zeros(size_, 1);
 
-    for (i = 1:size(incremental_values, 1) - 1)
+    for (i = 1:size_)
 
         difference = incremental_values(i + 1, :) - incremental_values(i, :);
 
-        if (difference < 0 && abs(difference) >= half_max_value)
-            incremental(i, :) = (max_value - incremental_values(i, :)) + incremental_values(i + 1, :);
-
-        elseif abs(difference) >= half_max_value
-            incremental(i, :) = (incremental_values(i + 1, :) - max_value) - incremental_values(i, :)
-
-        else
-            incremental(i, :) = difference;
-        end
+        incremental(i, :) = mod(difference + half_max_value, max_value) - half_max_value;
 
     endfor
 
