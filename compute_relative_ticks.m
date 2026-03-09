9a348053e40a1860eea0becbd4ebac68e591863f
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2026-03-09 10:28:07 +0100

    COmputing relative ticks to skip more measurements

--- compute_relative_ticks.m
+++ compute_relative_ticks.m
@@ -1,17 +1,24 @@
 %Re-working angle normalization formula
 
-function [incremental] = compute_relative_ticks(incremental_values, max_value)
+function [incremental,skipped] = compute_relative_ticks(incremental_values, max_value,time_values)
+    curr_time = time_values(1);
+    curr_idx = 1;
     half_max_value = max_value / 2;
-
-    size_ = size(incremental_values, 1) - 1;
-    incremental = zeros(size_, 1);
+    skipped = [];
+    size_ = size(incremental_values, 1) 
+    incremental = [];
 
     for (i = 1:size_)
+        if(abs(curr_time-time_values(i))>0.3)
+        
+            difference = incremental_values(i , :) - incremental_values(curr_idx, :);
 
-        difference = incremental_values(i + 1, :) - incremental_values(i, :);
-
-        incremental(i, :) = mod(difference + half_max_value, max_value) - half_max_value;
-
+            incremental = [incremental; mod(difference + half_max_value, max_value) - half_max_value];
+            curr_time = time_values(i);
+            curr_idx = i;
+        else
+            skipped = [skipped;i];
+        endif
     endfor
 
 endfunction
